0bd74ddb5d545509db8de0be47f96e715ab761a8
tejassun@mit.edu <tejassun@mit.edu>
2016-07-10 03:20:13 -0700

    octaves

--- src_new/create_chords.py
+++ src_new/create_chords.py
@@ -22,3 +22,26 @@ def create_chords(input_notes):
 				new_chord.highest.style = "FAST"
 				chords.append(new_chord)				
 	return chords
+
+
+# http://stackoverflow.com/questions/13926280/musical-note-string-c-4-f-3-etc-to-midi-note-value-in-python
+#Input is string in the form C#4, Db4, or F3. If your implementation doesn't use the hyphen, 
+#just replace the line :
+#    letter = midstr.split('-')[0].upper()
+#with:
+#    letter = midstr[:-1]
+def MidiStringToInt(midstr):
+    Notes = [["C"],["C#","Db"],["D"],["D#","Eb"],["E"],["F"],["F#","Gb"],["G"],["G#","Ab"],["A"],["A#","Bb"],["B"]]
+    answer = 0
+    i = 0
+    #Note
+    letter = midstr[:-1]
+    for note in Notes:
+        for form in note:
+            if letter.upper() == form:
+                answer = i
+                break;
+        i += 1
+    #Octave
+    answer += (int(midstr[-1]))*12
+    return answer
